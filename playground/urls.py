d7db4b83912646be6fb5cc9a17c36cab367c1691
Esha-Noor <nooresha692@gmail.com>
2025-10-20 16:22:32 +0500

    Initial commit

---
+++ playground/urls.py
@@ -0,0 +1,8 @@
+#map urls to view functions
+from django.urls import path
+from . import views
+
+#urlconf
+urlpatterns=[
+    path('hello/', views.say_hello)
+]
---
+++ playground/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+# request -> response
+# request handler
+# action
+
+def say_hello(request):
+    "pull data, transform, send mainls etc"
+    return render(request, 'hello.html')
